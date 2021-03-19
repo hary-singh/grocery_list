3a7c389895b8c199afd5d7c0d4b79ccd89f0b5dd
LuLu DeHaan <luludehaan@gmail.com>
2021-03-18 19:39:57 -0700

    add item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import GroceryList from './components/groceries/GroceryList'
 import GroceryItem from './components/groceries/GroceryItem'
+import GroceryForm from './components/groceries/GroceryForm'
 class App extends Component{
   state = {
       groceries: [
@@ -10,7 +11,18 @@ class App extends Component{
       ]
   }
 
-  getId = () => {}
+  getId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+
+  addItem = (incomingItem) => {
+    const { groceries } = this.state
+    let newItem = { id: this.getId(), ...incomingItem }
+    this.setState({ groceries: [...groceries, newItem ]})
+  }
+
 
   updateComplete = ( id ) => {
     const { groceries } = this.state
@@ -32,6 +44,7 @@ render() {
   return (
     <><div className="bg">
     <h1 className="text-center display-2"> Grocery List: </h1><hr />
+    <GroceryForm addItem={this.addItem} />
     <GroceryList groceries={groceries} updateComplete={this.updateComplete} />
     </div></>
   )
